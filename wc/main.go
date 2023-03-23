ee9ed7a67c93872052233938153e02fb551e9ade
Berny Linville <bernylinville@devopsthink.org>
2023-03-23 18:02:20 +0800

    Adding Bytes Flags

--- wc/main.go
+++ wc/main.go
@@ -11,15 +11,17 @@ import (
 func main() {
 	// Defining a boolean flag -l to count lines​ instead of words​
 	lines := flag.Bool("l", false, "Count lines")
+	// Defining a boolean flag -b to count bytes instead of lines​
+	bytes := flag.Bool("b", false, "Count bytes")
 	// Parsing the flags​ provided by the user​
 	flag.Parse()
 
 	// Calling the count function to count the number of words​
 	// received from the Standard Input and printing it out​
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader​ (such as files)
 	scanner := bufio.NewScanner(r)
 
@@ -27,6 +29,12 @@ func count(r io.Reader, countLines bool) int {
 	// the scanner split type to words (default is splited by lines)​
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+
+	// If the count bytes flag is set, we want to count bytes​ so we define
+	// the scanner split type to bytes​
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Define a counter​

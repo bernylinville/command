dc99da3c4845f332d6278c85a10d9aed40a32175
Berny Linville <bernylinville@devopsthink.org>
2023-03-31 17:22:10 +0800

    Handling Multiple Command-Line Options

---
+++ todo.v1/cmd/todo/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/bernylinville/command/todo.v1"
+)
+
+// Hardcoding the file name
+const todoFileName = ".todo.json"
+
+func main() {
+	// Parsing command line flags
+	task := flag.String("task", "", "task to be included in the ToDo list")
+	list := flag.Bool("list", false, "list all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	// Define an items list
+	l := &todo.List{}
+
+	// Use the Get method to read todo items from the file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on provided flags
+	switch {
+	// For no extra arguments, print the list
+	case *list:
+		// List current todo items
+		for _, item := range *l {
+			// Print only the undone items
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "invalid option")
+		os.Exit(1)
+	}
+}
